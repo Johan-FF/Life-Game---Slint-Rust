bb01e29247f674a80002a5270f954526c999a6f2
Johan FF <johan.artmat@gmail.com>
2023-09-07 22:50:30 -0500

    [UPDATE] main file with input data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
+  println!("Por favor introduce tu nombre: ");
+  let mut nombre: String = String::new();
+  std::io::stdin().read_line(&mut nombre).unwrap();
+  nombre = nombre.trim().to_string();
 
+  println!("Por favor introduce tu edad: ");
+  let mut edad: String = String::new();
+  std::io::stdin().read_line(&mut edad).unwrap();
+  let edad_numero: u8 = edad.trim().parse().unwrap();
 
+  println!("Hola, bienvenida {} de {}", nombre, edad_numero);
 }
