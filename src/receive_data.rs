3ceccb1bfa2b4f8622e486b65347446a8e8a7e58
Johan FF <johan.artmat@gmail.com>
2023-09-19 13:24:27 -0500

    [CREATE] receive data

---
+++ src/receive_data.rs
@@ -0,0 +1,13 @@
+fn main() {
+  println!("Please enter your name: ");
+  let mut name: String = String::new();
+  std::io::stdin().read_line(&mut name).unwrap();
+  name = name.trim().to_string();
+
+  println!("Please enter your age: ");
+  let mut age: String = String::new();
+  std::io::stdin().read_line(&mut age).unwrap();
+  let age_int: u8 = age.trim().parse().unwrap();
+
+  println!("Hello, welcome {}, {} years old", name, age_int);
+}
