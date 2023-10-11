d8ab96701664c21d115c85e7768a04b4862e83da
Johan FF <johan.artmat@gmail.com>
2023-10-10 23:18:28 -0500

    [CREATE/UPDATE] first version of calculator and add regex library

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,44 @@
+use regex::Regex;
+
 fn main() {
+  let re_add: Regex = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+  let re_mult: Regex = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+
+  println!("Please enter your expression: ");
+  let mut expression: String = String::new();
+  std::io::stdin().read_line(&mut expression).unwrap();
+
+  let mut caps;
+  let mut left_value: i32;
+  let mut right_value: i32;
+  let mut result: i32; 
+  let mut cap_expression: &str;
+  loop{
+    caps = re_mult.captures(expression.as_str());
+    if caps.is_none() {
+      break;
+    }
+    let caps = caps.unwrap();
+
+    cap_expression = caps.get(0).unwrap().as_str();
+    left_value = caps.get(1).unwrap().as_str().parse().unwrap();
+    right_value = caps.get(2).unwrap().as_str().parse().unwrap();
+    result = left_value * right_value;
+    expression = expression.replace(cap_expression, &result.to_string());
+  }
+  loop{
+    caps = re_add.captures(expression.as_str());
+    if caps.is_none() {
+      break;
+    }
+    let caps = caps.unwrap();
+
+    cap_expression = caps.get(0).unwrap().as_str();
+    left_value = caps.get(1).unwrap().as_str().parse().unwrap();
+    right_value = caps.get(2).unwrap().as_str().parse().unwrap();
+    result = left_value + right_value;
+    expression = expression.replace(cap_expression, &result.to_string());
+  }
+
+  print!("Result: {}", expression);
 }
